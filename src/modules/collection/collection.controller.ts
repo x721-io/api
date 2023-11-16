530c0ab70d8d1185b9a769a599e199973d0a6b07
Hieunm_u2u <hieunm@uniultra.xyz>
2023-11-16 14:56:44 +0700

    CRUD hieunm 16/11/2023

--- src/modules/collection/collection.controller.ts
+++ src/modules/collection/collection.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query , UseGuards} from '@nestjs/common';
 import { CollectionService } from './collection.service';
 import {CreateCollectionDto} from './dto/create-collection.dto';
 import { UpdateCollectionDto } from './dto/update-collection.dto';
-
+import { AuthenticationGuard } from '../auth/guards/auth.guard';
+import { GetCurrentUser } from 'src/decorators/get-current-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('collection')
 export class CollectionController {
   constructor(private readonly collectionService : CollectionService){}
 
   @Post()
-  create(@Body() createCollectionDto : CreateCollectionDto ) {
-    return this.collectionService.create(createCollectionDto);
+  @UseGuards(AuthenticationGuard)
+  create(@Body() createCollectionDto : CreateCollectionDto ,  @GetCurrentUser() user: User ) {
+    return this.collectionService.create(createCollectionDto , user);
   }
 
   @Get()
