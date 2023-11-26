2f76326e2a6c9ef3066c85a43283df767851f6a4
MacOs <dangkimbao1999@gmail.com>
2023-11-27 02:22:13 +0700

    handle nft details

--- src/modules/nft/dto/nft.dto.ts
+++ src/modules/nft/dto/nft.dto.ts
@@ -1,18 +1,18 @@
-import {TX_STATUS , UserNFT} from "@prisma/client";
-import { TraitEntity } from "../entities/trait.entity";
+import { TX_STATUS, UserNFT } from '@prisma/client';
+import { TraitEntity } from '../entities/trait.entity';
 
 export class NftDto {
-  id : string;
-  name : string;
-  txCreationHash : string;
-  ipfsHash : string;
-  traits : TraitEntity[];
-  createdAt : Date;
-  updatedAt : Date;
-  status : TX_STATUS;
-  tokenUri : string;
-  creatorId : string;
-  collectionId : string;
-  owners? : Array<UserNFT>
+  id: string;
+  name: string;
+  txCreationHash: string;
+  ipfsHash: string;
+  traits: TraitEntity[];
+  createdAt: Date;
+  updatedAt: Date;
+  status: TX_STATUS;
+  tokenUri: string;
+  creatorId: string;
+  collectionId: string;
+  owners?: Array<UserNFT>;
   sellInfo?: unknown;
 }
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,20 +1,20 @@
 import { User } from '@prisma/client';
 
-export class UserEntity implements User{
-    id: string;
-    email: string;
-    username: string;
-    publicKey: string;
-    signature: string;
-    signedMessage: string;
-    signDate: Date;
+export class UserEntity implements User {
+  id: string;
+  email: string;
+  username: string;
+  publicKey: string;
+  signature: string;
+  signedMessage: string;
+  signDate: Date;
 
-    constructor(partial: Partial<User>) {
-        Object.assign(this, partial);
-    }
-    acceptedTerms: boolean;
-    avatar: string;
-    createdAt: Date;
-    updatedAt: Date;
-    signer: string;
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+  acceptedTerms: boolean;
+  avatar: string;
+  createdAt: Date;
+  updatedAt: Date;
+  signer: string;
 }
