92685c95ca409ff9e2a32e8fc73525133fd62c85
MacOs <dangkimbao1999@gmail.com>
2023-11-27 16:18:11 +0700

    feat: marketplace event query

---
+++ src/modules/nft/dto/event-marketplace.dto.ts
@@ -0,0 +1,27 @@
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+import { QueryTraitDto } from './query-trait.dto';
+import { OffsetPaginationDto } from 'src/commons/definitions/OffsetPagination.input';
+import { SellStatus } from 'src/generated/graphql';
+import { CONTRACT_TYPE } from '@prisma/client';
+
+export class GetEventMarketplace extends OffsetPaginationDto {
+  @IsOptional()
+  @IsString()
+  nftId: string;
+  @IsString()
+  @IsOptional()
+  from: string;
+  @IsString()
+  @IsOptional()
+  to: string;
+  @IsString()
+  @IsOptional()
+  quoteToken: string;
+
+  @IsOptional()
+  @IsEnum(SellStatus)
+  event?: SellStatus;
+
+  @IsEnum(CONTRACT_TYPE)
+  type?: CONTRACT_TYPE;
+}
