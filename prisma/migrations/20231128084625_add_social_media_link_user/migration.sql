bf4be73733a19360b565ac852517eba0113e47ab
Hieunm_u2u <hieunm@uniultra.xyz>
2023-11-28 23:17:13 +0700

    Update and Get Profile User

---
+++ prisma/migrations/20231128084625_add_social_media_link_user/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "bio" VARCHAR(2000),
+ADD COLUMN     "discordLink" VARCHAR(1000),
+ADD COLUMN     "facebookLink" VARCHAR(1000),
+ADD COLUMN     "telegramLink" VARCHAR(1000),
+ADD COLUMN     "twitterLink" VARCHAR(1000),
+ADD COLUMN     "webURL" VARCHAR(1000);
