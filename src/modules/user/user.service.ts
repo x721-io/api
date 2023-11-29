08ef677b38c1099a6da0b80b47178fb879703bce
Hieunm_u2u <hieunm@uniultra.xyz>
2023-11-29 14:13:38 +0700

    Add Condition check exists Name Email Short Link

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,22 +1,22 @@
-import { 
-  Injectable, 
+import {
+  Injectable,
   NotFoundException,
   HttpException,
   HttpStatus
- } from '@nestjs/common';
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from '@prisma/client';
 import { GetAllUser } from './dto/get-all-user.dto';
-import  {UserEntity} from './entities/user.entity';
+import { UserEntity } from './entities/user.entity';
 import { validate as isValidUUID } from 'uuid'
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
-    // Remove few prop secret
+  // Remove few prop secret
   private minifyUserObject(user: any): any {
     const propertiesToRemove = ['signature', 'signer', 'signedMessage'];
     const minifiedUser = { ...user };
@@ -28,7 +28,7 @@ export class UserService {
     return minifiedUser;
   }
 
-  async findByPublicKey(publicKey: string) : Promise<any> {
+  async findByPublicKey(publicKey: string): Promise<any> {
     const account = await this.prisma.user.findFirst({
       where: {
         publicKey: publicKey,
@@ -95,29 +95,64 @@ export class UserService {
     try {
       let currentUserId = user?.id;
       let result: any = {};
-  
+
       if (!isValidUUID(input)) {
         result = await this.prisma.user.findFirst({
-          where: { OR : [{shortLink: input}, {signer: input.toLowerCase()}] },
+          where: {
+            OR: [
+              { shortLink: { equals: input, mode: 'insensitive' } },
+              { signer: { equals: input.toLowerCase(), mode: 'insensitive' } }
+            ]
+          },
         });
       } else {
         result = await this.prisma.user.findFirst({
-          where: {  id: input },
+          where: { id: input },
         });
       }
-  
+
+      if (!result) {
+        throw new NotFoundException();
+      }
+
       let response = (!currentUserId || currentUserId !== result.id)
-          ? this.minifyUserObject(result)
-          : result;
-  
+        ? this.minifyUserObject(result)
+        : result;
+
       return response;
     } catch (error) {
+      console.log(error);
       throw new HttpException(`${error.message}`, HttpStatus.BAD_REQUEST);
     }
   }
 
   async updateProfile(input: UpdateUserDto, user: User) {
-    try{
+    try {
+      const { email, username, shortLink } = input;
+      // Check for existing email
+      if (email) {
+        const checkExistEmail = await this.checkUserExistence('email', email, user.id);
+        if (checkExistEmail) {
+          throw new Error('Email already exists');
+        }
+      }
+
+      // Check for existing username
+      if (username) {
+        const checkExistUsername = await this.checkUserExistence('username', username, user.id);
+        if (checkExistUsername) {
+          throw new Error('Username already exists');
+        }
+      }
+
+      // Check for existing short link
+      if (shortLink) {
+        const checkExistShortLink = await this.checkUserExistence('shortLink', shortLink, user.id);
+        if (checkExistShortLink) {
+          throw new Error('Short Link already exists');
+        }
+      }
+
       let updateData = {
         email: input.email,
         username: input.username,
@@ -129,33 +164,36 @@ export class UserService {
         discordLink: input.discordLink,
         webURL: input.webURL,
         coverImage: input.coverImage,
-        shortLink : input.shortLink
+        shortLink: input.shortLink
       };
 
-      if(input && input.shortLink){
-        let checkExistShortLink = await this.prisma.user.findFirst({
-          where: {AND: [
-            {id : { not: user.id }},
-            {shortLink : input.shortLink}
-          
-          ]}
-        })
-        if(checkExistShortLink){
-          throw new Error('Short Link already exists');
-        } 
-      }else{
-        delete updateData.shortLink
+      // Remove shortLink if not provided
+      if (!shortLink) {
+        delete updateData.shortLink;
       }
-      
-      let response = await this.prisma.user.update({
-          where: {
-            id: user.id
-          },
-          data: updateData
-        })
-        return response;
-    }catch(error){
+
+      return await this.prisma.user.update({
+        where: {
+          id: user.id
+        },
+        data: updateData
+      })
+
+    } catch (error) {
       throw new HttpException(`${error.message}`, HttpStatus.BAD_REQUEST);
     }
+  }
+
+  async checkUserExistence(field: string, value: string, userId: string) {
+    const condition = {
+      id: { not: userId },
+      [field]: value,
+    };
+
+    const existingUser = await this.prisma.user.findFirst({
+      where: { AND: [condition] },
+    });
+
+    return !!existingUser;
   }
 }
