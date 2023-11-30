7f325f80091f38fea579d2b4a4ad549a1d981431
MacOs <dangkimbao1999@gmail.com>
2023-11-30 14:10:49 +0700

    feat: collection trait information format

--- src/modules/nft/trait.service.ts
+++ src/modules/nft/trait.service.ts
@@ -1,24 +1,69 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+export type TraitGeneralInfo = {
+  key: string;
+  count: number;
+  traits: {
+    value: string;
+    count: number;
+  }[];
+};
 @Injectable()
 export class TraitService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async findUniqueTraitsInCollection(collectionId: string): Promise<string[]> {
-        const nfts = await this.prisma.nFT.findMany({
-            where: {collectionId},
-            include: {
-                traits: true,
-            }
-        })
+  async findUniqueTraitsInCollection(
+    collectionId: string,
+  ): Promise<TraitGeneralInfo[]> {
+    const nfts = await this.prisma.nFT.findMany({
+      where: { collectionId },
+      include: {
+        traits: true,
+      },
+    });
 
-        const traitTypes = new Set<string>();
-        nfts.forEach(nft => {
-            nft.traits.forEach(trait => {
-              traitTypes.add(trait.trait_type);
-            });
-          });
-        return Array.from(traitTypes);
+    const traitInfo = this.summarizeTraits(nfts);
+    return traitInfo;
+  }
+
+  summarizeTraits(data: any[]) {
+    if (data.length === 0) {
+      return [];
     }
+    const traitInfo = new Map<
+      string,
+      { count: number; traits: Map<string, number> }
+    >();
+
+    data.forEach((nft) => {
+      nft.traits.forEach((trait: any) => {
+        if (!traitInfo.has(trait.trait_type)) {
+          traitInfo.set(trait.trait_type, {
+            count: 0,
+            traits: new Map<string, number>(),
+          });
+        }
+
+        const typeInfo = traitInfo.get(trait.trait_type);
+        if (typeInfo) {
+          typeInfo.count++;
+
+          const valueMap = typeInfo.traits;
+          if (!valueMap.has(trait.value)) {
+            valueMap.set(trait.value, 0);
+          }
+          valueMap.set(trait.value, valueMap.get(trait.value) + 1);
+        }
+      });
+    });
+
+    return Array.from(traitInfo).map(([key, info]) => ({
+      key: key,
+      count: info.count,
+      traits: Array.from(info.traits).map(([value, count]) => ({
+        value,
+        count,
+      })),
+    }));
+  }
 }
