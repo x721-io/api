da12a3eb938270ea7644191260ffca9d471e8bae
Hieunm_u2u <hieunm@uniultra.xyz>
2023-12-06 23:21:06 +0700

    Add Avatar User Profile

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,28 +1,37 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNotEmpty, IsString , IsOptional, IsEnum , IsNumber , IsNumberString, Min, IsBoolean , IsDefined } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+  IsEnum,
+  IsNumber,
+  IsNumberString,
+  Min,
+  IsBoolean,
+  IsDefined,
+} from 'class-validator';
 
-export class UpdateUserDto  {
-    email: string;
-    username: string;
-    acceptedTerms: boolean;
-    @IsOptional()
-    bio : string
-    @IsOptional()
-    facebookLink : string;
-    @IsOptional()
-    twitterLink : string;
-    @IsOptional()
-    telegramLink : string;
-    @IsOptional()
-    discordLink : string;
-    @IsOptional()
-    webURL : string;
-    @IsOptional()
-    avatar : string;
-    @IsOptional()
-    coverImage : string
-    @IsOptional()
-    shortLink : string
+export class UpdateUserDto {
+  email: string;
+  username: string;
+  acceptedTerms: boolean;
+  @IsOptional()
+  bio: string;
+  @IsOptional()
+  facebookLink: string;
+  @IsOptional()
+  twitterLink: string;
+  @IsOptional()
+  telegramLink: string;
+  @IsOptional()
+  discordLink: string;
+  @IsOptional()
+  webURL: string;
+  @IsOptional()
+  avatar: string;
+  @IsOptional()
+  coverImage: string;
+  @IsOptional()
+  shortLink: string;
 }
-
