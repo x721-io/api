e20df220fc1ecfc9b21a434b2c156f906555831d
hieunm_u2u <hieunm@uniultra.xyz>
2023-12-12 21:28:55 +0700

    Create User Subscribe Staking Project

--- src/modules/launchpad/launchpad.controller.ts
+++ src/modules/launchpad/launchpad.controller.ts
@@ -6,10 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { LaunchpadService } from './launchpad.service';
 import { CreateLaunchpadDto } from './dto/create-launchpad.dto';
 import { UpdateLaunchpadDto } from './dto/update-launchpad.dto';
+import { CheckStakingDto } from './dto/check-staking.dto';
+import { AuthenticationGuard } from '../auth/guards/auth.guard';
+import { User } from '@prisma/client';
+import { GetCurrentUser } from 'src/decorators/get-current-user.decorator';
 
 @Controller('launchpad')
 export class LaunchpadController {
@@ -28,5 +33,14 @@ export class LaunchpadController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.launchpadService.findOne(id);
+  }
+
+  @Post()
+  @UseGuards(AuthenticationGuard)
+  async checkStak(
+    @Body() inputStaking: CheckStakingDto,
+    @GetCurrentUser() user: User,
+  ) {
+    return await this.launchpadService.checkStaking(inputStaking, user);
   }
 }
