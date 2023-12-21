92cfcf2df472b458c396bb5f508d807adffa973f
Phuc Nguyen <phucnguyen.it@gmail.com>
2023-12-21 05:31:03 +0000

    Add get env from aws

---
+++ build/get_ssm_value.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# To use just set a variable with <target_env_var>=SSM:<ssm_parameter_store_path>
+# e.g. database_password=SSM:/prod/myservice/database-password
+# remove current env
+rm -rf .env
+
+function export_key() {
+    COMMAND="export $1='$2'"
+    eval ${COMMAND}
+    echo "$1=$2" >> .env
+}
+function read_properties() {
+  file="$1"
+  while IFS="=" read -r key value; do
+    echo "$key=\"$value\""
+    if [[ $value == *"SSM:"* ]]; then
+      ssm_key="${value/SSM:/}"
+      get_parameter $key $ssm_key
+    else
+      export_key $key $value
+    fi
+  done < "$file"
+}
+
+
+function get_parameter() {
+    ENV_VAR_NAME=$1
+    SSM_PARAM_NAME=$2
+
+    echo "Getting parameter $SSM_PARAM_NAME from SSM parameter store if it exists and setting into the variable $ENV_VAR_NAME"
+
+    SSM_VALUE=`aws ssm get-parameters --with-decryption --names "${SSM_PARAM_NAME}"  --query 'Parameters[*].Value' --output text`
+
+    if [[ -z "$SSM_VALUE" ]]; then
+        echo "Must provide $ENV_VAR_NAME in environment" 1>&2
+        exit 1
+    fi
+    export_key $ENV_VAR_NAME $SSM_VALUE
+}
+
+filename="./$1.env"
+
+read_properties $filename
