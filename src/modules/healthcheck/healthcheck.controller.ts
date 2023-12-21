10fc7af91fc8fc1d153ca9803258a52d557672d3
MacOs <dangkimbao1999@gmail.com>
2023-12-21 18:30:19 +0700

    fix: fix healthcheck

--- src/modules/healthcheck/healthcheck.controller.ts
+++ src/modules/healthcheck/healthcheck.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode } from '@nestjs/common';
 
 @Controller('/')
 export class HealthcheckController {
   @Get('/')
   @HttpCode(200)
+  @Header(
+    'Cache-Control',
+    'no-store, no-cache, must-revalidate, proxy-revalidate',
+  )
+  @Header('Pragma', 'no-cache')
   healthCheck() {
     // You can add additional checks here if needed
     return { status: 'ok' };
