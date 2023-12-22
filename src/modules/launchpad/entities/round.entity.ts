8efb7852fc080cb5d87eacf5906ab8fbe81ec635
MacOs <dangkimbao1999@gmail.com>
2023-12-22 15:30:09 +0700

    fix: hotfix type

--- src/modules/launchpad/entities/round.entity.ts
+++ src/modules/launchpad/entities/round.entity.ts
@@ -2,7 +2,7 @@ import { ProjectRound } from '@prisma/client';
 import { CollectionEntity } from 'src/modules/collection/entities/collection.entity';
 
 export class RoundEntity implements ProjectRound {
-  price: bigint;
+  price: string;
   maxPerWallet: number;
   totalNftt: number;
   claimableStart: Date;
