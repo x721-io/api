ecf1f21010064a00dfb3569ddb56508111ee65ea
hieunm_U2U <hieunm@uniultra.xyz>
2024-10-25 11:47:29 +0700

    [Update] Check log

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ function matchRegexArray(arr: string[], str: string): boolean {
 async function bootstrap() {
   const whitelist = process.env.ALLOWED_DOMAIN.split(',');
   const app = await NestFactory.create(AppModule);
+  console.log('🚀 ~ bootstrap ~ whitelist:', whitelist);
 
   app.use(cookieParser());
 
