b0d5327ae1dd938b58951a63547c218538e12290
hieunm_U2U <hieunm@uniultra.xyz>
2024-11-12 16:49:13 +0700

    [Fix] Docker file

--- Dockerfile
+++ Dockerfile
@@ -19,6 +19,8 @@ RUN yarn codegen
 
 RUN npx prisma generate
 
+RUN npx ts-node prisma/seed.ts
+
 # Build the NestJS project
 RUN yarn build
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,5 @@
 # Run database migrations
 npx prisma migrate deploy
 
-npx ts-node prisma/seed.ts
 # Start the NestJS server
 npx prisma studio & yarn start:prod
