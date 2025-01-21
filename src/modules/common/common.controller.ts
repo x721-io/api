4f78babecb3427e9b332f0357ae57f383abe978e
hieunm_U2U <hieunm@uniultra.xyz>
2025-01-21 09:51:58 +0700

    [Update] Add Get IPFS Same Path Different TokenID

--- src/modules/common/common.controller.ts
+++ src/modules/common/common.controller.ts
@@ -41,6 +41,15 @@ export class CommonController {
     return await this.commonService.getFileFromIpfsPath(ipfsPath, res);
   }
 
+  // Get Medata From IPFS Same ipfsPath/tokenid
+  @Get('/ipfs-server/:ipfsPath/:id')
+  async getIPFS(
+    @Param('ipfsPath') ipfsPath: string,
+    @Res() res: Response,
+  ): Promise<any> {
+    return await this.commonService.getFileFromIpfsPath(ipfsPath, res);
+  }
+
   @Get('get-ipfs')
   async getIpfs(@Query('hash') hash: string) {
     return await this.commonService.getFromIpfs(hash);
